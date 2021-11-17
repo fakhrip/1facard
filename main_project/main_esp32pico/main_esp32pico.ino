2f37b6bce2158763b2f5ca571cdea8327f2d5dad
fakhrip <fakhriputra123s@gmail.com>
2021-11-17 22:44:26 +0700

    add spiffs support

--- main_project/main_esp32pico/main_esp32pico.ino
+++ main_project/main_esp32pico/main_esp32pico.ino
@@ -1,4 +1,6 @@
+#include <FS.h>
 #include <WiFi.h>
+#include <SPIFFS.h>
 #include <Arduino.h>
 #include <esp_now.h>
 #include <FunctionalInterrupt.h>
@@ -144,7 +146,7 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *received_data, int len)
         switch (receive_payload.data_type)
         {
         case 1:
-            /* code */
+            writeFile(SPIFFS, "/qrcode_data.txt", payload_data);
             break;
 
         case 2:
@@ -166,6 +168,12 @@ void setup()
     Serial.begin(115200);
     while (!Serial)
         ;
+
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    }
 
     Serial.printf("Starting ESP32PICO .....");
     WiFi.mode(WIFI_STA);
@@ -201,4 +209,127 @@ void loop()
 {
     red_button.checkPressed();
     green_button.checkPressed();
+}
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
+{
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if (!root)
+    {
+        Serial.println("− failed to open directory");
+        return;
+    }
+    if (!root.isDirectory())
+    {
+        Serial.println(" − not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file)
+    {
+        if (file.isDirectory())
+        {
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if (levels)
+            {
+                listDir(fs, file.name(), levels - 1);
+            }
+        }
+        else
+        {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char *path)
+{
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if (!file || file.isDirectory())
+    {
+        Serial.println("− failed to open file for reading");
+        return;
+    }
+
+    Serial.println("− read from file:");
+    while (file.available())
+    {
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file)
+    {
+        Serial.println("− failed to open file for writing");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("− file written");
+    }
+    else
+    {
+        Serial.println("− frite failed");
+    }
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if (!file)
+    {
+        Serial.println("− failed to open file for appending");
+        return;
+    }
+    if (file.print(message))
+    {
+        Serial.println("− message appended");
+    }
+    else
+    {
+        Serial.println("− append failed");
+    }
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2)
+{
+    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
+    if (fs.rename(path1, path2))
+    {
+        Serial.println("− file renamed");
+    }
+    else
+    {
+        Serial.println("− rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char *path)
+{
+    Serial.printf("Deleting file: %s\r\n", path);
+    if (fs.remove(path))
+    {
+        Serial.println("− file deleted");
+    }
+    else
+    {
+        Serial.println("− delete failed");
+    }
 }
